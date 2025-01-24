531bdb97dc00121f831d1298f8f290444dbf0276
AbhishekPatil313 <abhishekpatil3311@gmail.com>
2025-01-24 13:16:52 +0530

    practice

---
+++ arr.js
@@ -0,0 +1,7 @@
+let str = "hello this is abhishek"
+console.log(str);
+let splitted_str = str.split(' '); //return an array 
+console.log(splitted_str);
+let reversed = splitted_str.reverse();
+let joined = reversed.join('');
+console.log(joined);
--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 console.log("hello")
+console.table(["hello","hi","this"]);
+const arr=[12,34,4,3,2];    
+/* not to use var for declaration of variable   */
+
+console.log(typeof("hello"));
+console.log(typeof(undefined));
+console.log(typeof(null));
+
+let data;
+
+if(data){
+    console.log("the data was undefined");
+}
+
+let n = 1235;
+while(n>0){
+    let digit = Math.floor(n%10);
+    console.log(digit)
+    n=n/10;
+}
